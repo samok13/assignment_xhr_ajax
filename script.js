e28f6c2e56f6a46ced8f1427cef306dadfdbce74
Sam O'Keefe <samok13@gmail.com>
2016-03-17 10:04:33 -0700

    created promise object

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ $.ajax = function(options) {
 
   var xhr = new XMLHttpRequest();
 
+  var promise = new Promise(function(resolve, reject){
+    xhr.addEventListener( "load", function(){
+      switch (this.status) {
+        case 200:
+        case 201:
+        case 202:
+        options.success(this.responseText);
+        resolve(this.responseText);
+        break;
+        default:
+        options.error(this.responseText);
+        reject(this.responseText);
+        break;
+      }
+    });
+
+    xhr.addEventListener( "error", function(){
+      options.error(this.responseText);
+      reject(this.responseText);
+    });
+
+  })
+
+  xhr.open( options.method || options.type, options.url, options.async || true);
+
   for(header in options.headers) {
     var value = options.headers[header]
     console.log("Adding header:");
@@ -13,27 +38,42 @@ $.ajax = function(options) {
     xhr.setRequestHeader(header, value)
   }
 
-  xhr.addEventListener( "load", function(){
-    switch (this.status) {
-      case 200:
-        options.success(this.responseText);
-        break;
-      default:
-        options.error(this.responseText);
-        break;
-    }
-  });
-
-  xhr.addEventListener( "error", function(){
-    options.error(this.responseText);
-  });
-
-  xhr.open( options.method || options.type, options.url, options.async || true);
   xhr.send( options.data );
+
+  return promise;
 }
 
+//Convenience Methods
+$.get = function(url, success){
+  $.ajax({
+    method: "GET",
+    url: url,
+    success: success
+  })
+}
 
-$.ajax({
+$.get("http://jsonplaceholder.typicode.com/users", function(response) {
+    console.log("Success: " + response);
+  })
+
+$.post = function(url, data, success){
+  $.ajax({
+    method: 'POST',
+    url: url,
+    data: data,
+    success: success
+  })
+}
+
+$.post("http://jsonplaceholder.typicode.com/posts",{
+   title: 'foo',
+   body: 'bar',
+   userId: 1
+ }, function(data){
+  console.log(data);
+})
+
+var promise = $.ajax({
   method: "GET",
   url: "http://jsonplaceholder.typicode.com/users",
   success: function(response) {
@@ -46,6 +86,10 @@ $.ajax({
     name: "Whee",
     hooray: "Hahahaha"
   }
+})
+
+promise.then(function(val){
+  console.log('Promise Returned.' + val);
 })
 
 
